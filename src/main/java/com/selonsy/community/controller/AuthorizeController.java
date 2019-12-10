b617b182b17db9b9491222036fb744e5e2ba69c9
selonsy <selonsy@gmail.com>
2019-12-10 20:37:21 +0800

    从配置文件中读取信息

--- src/main/java/com/selonsy/community/controller/AuthorizeController.java
+++ src/main/java/com/selonsy/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.selonsy.community.dto.GithubUser;
 import com.selonsy.community.provider.GithubProvider;
 //import org.graalvm.compiler.nodes.memory.Access;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,14 +23,22 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    //@Value 注解，可以从 application.properties 文件中读取配置信息。
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("5550a9bd21e25864cf23");
-        accessTokenDTO.setClient_secret("4a018ea12861a895ee0f1dd69018f7697181accb");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
