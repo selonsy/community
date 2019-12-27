d49093ac0cfb940ea100ebabbd000caf7a55e8ce
selonsy <selonsy@gmail.com>
2019-12-27 14:26:02 +0800

    init by codedrinker

--- src/main/resources/db/migration/V1__Create_person_table.sql
+++
@@ -1,9 +0,0 @@
-create table USER
-(
-    ID           INT auto_increment primary key not null ,
-    NAME         VARCHAR(50),
-    ACCOUNT_ID   VARCHAR(100),
-    TOKEN        VARCHAR(36),
-    GMT_CREATE   BIGINT,
-    GMT_MODIFIED BIGINT
-);
